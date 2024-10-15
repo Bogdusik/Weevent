5ef29d56d03f0e0c0e15cfcc28678086f496b7ea
Bogdusik <bogdyn6@gmail.com>
2024-10-15 11:38:39 +0100

    Actual working FAQ page
    implement code in faq.html file and finally our code is working as needed

--- faq.js
+++ faq.js
@@ -1,19 +1,14 @@
-"use strict"
-
 var coll = document.getElementsByClassName("collapsible");
 
 for (var i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
-
+    
     var content = this.nextElementSibling;
-
     if (content.style.maxHeight) {
-      // Collapse the section
       content.style.maxHeight = null;
     } else {
-      // Expand the section
-      content.style.maxHeight = content.scrollHeight + "px";
+      content.style.maxHeight = content.scrollHeight + "px"; // Expand to content height
     }
   });
 }
