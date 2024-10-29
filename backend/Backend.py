eb1d1b37050b76a8dc4e2d32b501a6c6df96f4d8
MartinMacD <149177224+MartinMacD@users.noreply.github.com>
2024-10-29 10:26:28 +0000

    Added API Key file reader

--- backend/Backend.py
+++ backend/Backend.py
@@ -1,5 +1,10 @@
 import django
+import json 
+import requests
 
+apiKey = open(r"C:\Users\Martin\Desktop\EventAPI.txt", "r")
+print (apiKey.read())
 print(django.get_version())
 
-print("Hello World")
+
+
